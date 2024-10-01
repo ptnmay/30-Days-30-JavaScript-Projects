3e8adc9f745deac492e37721736537418a23203a
Pittinan Saeyang <pittinansaeyang@gmail.com>
2024-10-02 02:59:39 +0700

    in-correct ans

---
+++ D3_QuizApp/script.js
@@ -0,0 +1,78 @@
+const questions = [
+    {
+        question: "The larget animal on the world?",
+        answers: [
+            { text: "Shark", correct: false },
+            { text: "Blue whale", correct: true },
+            { text: "Elephant", correct: false },
+            { text: "Giraffe", correct: false },
+        ]
+    },
+    {
+        question: "My fav food?",
+        answers: [
+            { text: "Pizza", correct: true },
+            { text: "Bueger", correct: false },
+            { text: "Vegan food", correct: false },
+            { text: "Tom yum kung", correct: false },
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const ansBtn = document.getElementById("ans-btn");
+const nextBtn = document.getElementById("next-btn");
+
+let curQuesIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    curQuesIndex = 0;
+    score = 0;
+    nextBtn.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+    let curQues = questions[curQuesIndex];
+    let questionNo = curQuesIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + curQues.question;
+
+    curQues.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        ansBtn.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAns);
+    })
+}
+
+function resetState() {
+    nextBtn.style.display = "none";
+    while (ansBtn.firstChild){
+        ansBtn.removeChild(ansBtn.firstChild);
+    }
+}
+
+function selectAns(e) {
+    const selectBtn = e.target;
+    const isCorrect = selectBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectBtn.classList.add("correct");
+    } else {
+        selectBtn.classList.add("incorrect")
+    }
+    Array.from(ansBtn.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextBtn.style.display = "block";
+}
+
+startQuiz();
